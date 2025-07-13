11b2264aecd090d73e4fad3c5e2bf220c18589ab
potato03kth <potatokth@gmail.com>
2025-07-14 02:41:30 +0900

    inititalization of repository
    this is inner repository of full unity project

---
+++ ButtonListener.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ButtonListener : MonoBehaviour
+{
+    public void OnClickButton(){
+        Debug.Log("you just pressed button and this called from Debug.Log");
+        print("you just pressed button and this called from print");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("hello");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
